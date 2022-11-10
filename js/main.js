03d7d4d732ff66ca014ac2ed4ec0162510f71bed
eeeebta <eeeebta@gmail.com>
2022-11-10 16:37:12 -0500

    my data

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // load all data using promises
 let promises = [
-    d3.csv("data/music_data.csv"),
-    d3.csv("data/subjective_all.csv")
+    d3.csv("data/music_data.csv")
+    d3.csv("data/music_data.csv")
 ];
 
 Promise.all(promises)
