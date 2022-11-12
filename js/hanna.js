c1790121f49b476657f7ad75a2765d3572f6237b
Hanna Pak <hannacarlie@gmail.com>
2022-11-12 14:19:59 -0500

    work on hanna.js

--- js/hanna.js
+++ js/hanna.js
@@ -5,9 +5,9 @@ class musicVis {
 
         // temporary color scheme (DELETE LATER)
         this.tempcolors = d3.schemeSet3;
-        this.pieData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 
         // key arrays for circle of fifths pie chart
+        this.pieData = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
         this.keys_short = ["C", "G", "D", "A", "E", "Cb/B", "Gb/F#", "Db/C#", "Ab", "Eb", "Bb", "F"];
         this.keys_long = ["C major/ A minor", "G major/ E minor", "D major/ B minor", "A major/ F# minor", "E major/ C# minor", "B major/ G# minor", "F#/Gb major/ D#/Eb minor",
                             "Db major/ Bb minor", "Ab major/ F minor", "Eb major/ C minor", "Bb major/ G minor", "F major/ D minor"];
@@ -45,7 +45,7 @@ class musicVis {
         vis.pieChartGroup = vis.svg
             .append("g")
             .attr("class", "pieChart_CoF")
-            .attr("transform", "translate(" + vis.outerRadius + ", " + vis.outerRadius + ") rotate(-15)")
+            .attr("transform", "translate(" + vis.outerRadius + ", " + vis.outerRadius + ") rotate (-15)")
 
         // define pie layout
         vis.pie = d3.pie()
@@ -58,30 +58,11 @@ class musicVis {
             .innerRadius(1)
             .outerRadius(vis.outerRadius);
 
-        // bind data
-        vis.arcs = vis.pieChartGroup.selectAll(" .arc")
-            .data(vis.pie(vis.pieData))
-
-        // append paths
-        vis.arcs.enter()
-            .append("path")
-            .attr("d", vis.arc)
-            .style("fill", (d, i) => {
-                return this.tempcolors[i];
-            });
-
-
-        vis.arcs
-            .data(vis.keys_short)
-            .enter()
-            .append("text")
-            .text(d => d)
-            .attr("transform", (d, i) => {
-                console.log(d);
-
-            })
-            .attr("text-anchor", "end")
-            .attr("font-size", 12);
+        // append tooltip
+        vis.tooltip = d3.select("body")
+            .append("div")
+            .attr("class", "tooltip")
+            .attr("id", "tooltip_songs")
 
         this.wrangleData()
     }
@@ -96,6 +77,46 @@ class musicVis {
 
     updateVis() {
         let vis = this;
+
+        // bind data
+        vis.arcs = vis.pieChartGroup.selectAll(" .arc")
+            .data(vis.pie(vis.pieData))
+
+        // append paths & make array of centroid locations
+        vis.centroid = [];
+        vis.arcs.enter()
+            .append("path")
+            .attr("d", vis.arc)
+            .style("fill", (d, i) => {
+                // populate array of centroid locations
+                vis.centroid.push(vis.arc.centroid(d));
+                return vis.tempcolors[i];
+            })
+            .on("mouseover", function(event, d) {
+                d3.select(this)
+                    .attr("stroke-width", "2px")
+                    .attr("stroke", "black")
+
+            })
+            .on("mouseout", function(event, d) {
+                d3.select(this)
+                    .attr("stroke-width", "0px")
+            })
+
+        // append key labels @ centroid coordinates
+        vis.arcs
+            .data(vis.keys_short)
+            .enter()
+            .append("text")
+            .attr("class", "key-labels")
+            .text(d => d)
+            .attr("text-anchor", "middle")
+            .attr("font-size", 12)
+            .attr("transform", (d, i) => {
+                return "translate(" + (1.8 * vis.centroid[i][0]) + ", " + (1.8 * vis.centroid[i][1]) + ") rotate(15)"
+            })
+
+
 
     }
 }
