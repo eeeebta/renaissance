5e09b20605906748d160a307165484b606248700
curiousgiorgos <geomiliaras@gmail.com>
2022-11-29 16:20:19 -0500

    Small additions

--- js/figures.js
+++ js/figures.js
@@ -65,7 +65,7 @@ class figures {
             vis.imgs.push(vis.svg
                 .append("svg:image")
                 .attr("href", "assets/images/figures.png")
-                .attr("x", (i%20)*20+300)
+                .attr("x", (i%20)*20)
                 .attr("y", Math.floor(i/20)*20 )
                 .attr("width", "20")
                 .attr("height", "20")
@@ -74,33 +74,66 @@ class figures {
                     console.log(d)
                 })
                 .transition()
-                .duration(5000));
+                .duration(2000));
         }
         vis.anchors = []
         vis.tracks = []
-        tracksNames.slice(0,4).forEach(function(d, index){
-            vis.anchors.push([(index+1) * 140 + 50, 150])
+        tracksNames.forEach(function(d, index){
+            let curr_x,
+                curr_y
             vis.tracks.push(
                 vis.svg.append("text")
                     .attr('class', 'tracks')
-                    .attr('x', (index+1) * 140 + 50)
-                    .attr('y', 150)
-                    .text(d)
+                    .attr('x', function() {
+                        if(index>=8){
+                            curr_x = (index - 8) * 120
+                            return (index - 8) * 120
+                        }
+                        curr_x = (index*120)
+                        return (index*120)
+                    })
+                    .attr('y', function(){
+                        if(index>=8){
+                            console.log('uwu')
+                            curr_y = 400
+                            return 400
+                        }
+                        curr_y = 150
+                        return 150})
+                    .text(function(){
+                        if(d === "MOVE (feat. Grace Jones & Tems)"){
+                            return 'MOVE'
+                        }
+                        if(d === "ENERGY (feat. Beam)"){
+                            return 'ENERGY'
+                        }
+                        return d
+                    })
                     .attr('fill','white')
                     .attr('font-size', '10px'))
+            vis.anchors.push([curr_x,curr_y])
         })
         let curr = 0
         vis.imgs[0].attr('x',0)
 
-        tracksNames.slice(0,4).forEach(function(d,index){
+        tracksNames.forEach(function(d,index){
             let fig = tracksFigures[d]
             let x = vis.anchors[index][0]
             let y = vis.anchors[index][1]
             for(let i =0; i <= fig;i++){
                 vis.imgs[curr+i]
-                    .attr('x',x+(i- 3*Math.floor(i/3))*20)
-                    .attr('y',y+20+Math.floor(i/3)*20)
-                console.log('hi')
+                    .attr('x',function(){
+                        if(i>=8){
+                            return x+((i)- 3*Math.floor(i/3))*20-2
+                        }
+                        return x+(i- 3*Math.floor(i/3))*20-2
+                    })
+                    .attr('y',function(){
+                        if(i>=8){
+                            return y+20+Math.floor((i)/3)*20
+                        }
+                        return y+20+Math.floor(i/3)*20
+                    })
             }
             curr += fig
         })
